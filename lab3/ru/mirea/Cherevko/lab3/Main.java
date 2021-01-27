9d49b40c65cf750e81fbe0e74b26beac4cd29249
igorcherevko <72574021+igorcherevko@users.noreply.github.com>
2021-01-27 19:02:42 +0300

    Add files via upload

---
+++ lab3/ru/mirea/Cherevko/lab3/Main.java
@@ -0,0 +1,9 @@
+package ru.mirea.Cherevko.lab3;
+
+import ru.mirea.Cherevko.lab3.LabExample;
+
+public class Main {
+    public static void main(String[] args) {
+        new LabExample();
+    }
+}
